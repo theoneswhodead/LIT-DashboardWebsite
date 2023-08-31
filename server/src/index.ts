dc7f19b434cb17b6505b5b8e640eed361c880787
NyMek <nymek.kontakt@gmail.com>
2023-08-31 14:55:05 +0200

    connecting to DB and Listening

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello World")
+import express, { Express, Request, Response } from "express";
+import mongoose from "mongoose";
+import 'dotenv/config'
+
+const app: Express = express()
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=> {
+        app.listen(process.env.PORT, ()=> {
+            console.log("Połączono z Bazą danych i rozpoczęto nasłuchiwanie na porcie ", process.env.PORT)
+        })
+    })
+    .catch((err: Error) => {
+        console.log(err)
+    })
+
